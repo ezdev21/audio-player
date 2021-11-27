4f68332731d55b101cf77c578f00f9f793697811
gech11 <getachewfikadu77@gmail.com>
2021-11-27 22:07:45 +0300

    initial commit

--- lib/detail_audio.dart
+++ lib/detail_audio.dart
@@ -10,6 +10,33 @@ class DetailAudio extends StatefulWidget {
 class _DetailAudioState extends State<DetailAudio> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final double screenHeight=MediaQuerry.of(context).size.height;
+    final double screenWidth=MediaQuerry.of(context).size.width;
+    return Scaffold(
+      appBar:AppBar(
+       title:Text('music player')
+      ),
+      body:Stack(
+       children:[
+         positioned(
+          top:0,
+          left:0,
+          right:0,
+          hight:screenHeight/3, 
+          child:Container(
+           color:Colors.blue[500],
+          )
+         ),
+         positioned(
+          top:0,
+          left:0,
+          right:0,
+          child:AppBar(
+            
+          )
+         ),
+       ]
+      )
+    );
   }
 }
